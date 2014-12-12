899bf17d9cb486189d122725c3f8103f8fbb8e3e
Yosuke Matsusaka <yosuke.matsusaka@gmail.com>
2014-12-12 17:46:03 +0900

    update build script to generate both english and japanese html

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+rm -r _build
+make gettext
 sphinx-intl update -p _build/locale
 sphinx-intl build
-rm -r _build/html
 make -e SPHINXOPTS="-D language='ja'" html
-mv _build/html _build/html-ja
+mv _build/html _html-ja
 make html
+mv _html-ja _build/html-ja
