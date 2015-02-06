f83c56295c0ba844997771a206d37528e868a81a
Yosuke Matsusaka <yosuke.matsusaka@gmail.com>
2015-02-06 15:54:56 +0900

    use doxygen xml output from installed hrpsys-base

--- .travis.sh
+++ .travis.sh
@@ -2,8 +2,9 @@
 
 set -x
 
+sudo add-apt-repository -y ppa:hrg/daily
 sudo apt-get update -qq
-sudo apt-get install -qq -y python-pip graphviz
+sudo apt-get install -qq -y python-pip graphviz pkg-config hrpsys-base
 sudo pip install -r requires.txt
 
 source ./build.sh
